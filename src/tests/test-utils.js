d857ad8935b5fb7347dfb41f23f72f718176654f
sardq <dreod@mail.ru>
2026-06-03 22:23:44 +0400

    ы

--- src/tests/test-utils.js
+++ src/tests/test-utils.js
@@ -1,22 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { AuthContent } from "../AuthContent";
 import { BrowserRouter } from "react-router-dom";
 
 export const renderWithAuth = (
-  ui,
-  {
-    role = "STUDENT",
-    email = "test@mail.com",
-  } = {}
+  ui
 ) => {
   return render(
-    <AuthContent.Provider
-      value={{ role, email, setRole: jest.fn(), setEmail: jest.fn() }}
-    >
       <BrowserRouter>
         {ui}
       </BrowserRouter>
-    </AuthContent.Provider>
   );
 };
